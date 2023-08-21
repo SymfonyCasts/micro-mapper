4fb75a7005a26efb03ad01dabbc842d9a1a4a096
Ryan Weaver <ryan@symfonycasts.com>
2023-08-21 09:29:06 -0400

    Adding phpdoc generics for the interfaces

--- src/MapperInterface.php
+++ src/MapperInterface.php
@@ -14,11 +14,26 @@ namespace Symfonycasts\MicroMapper;
  *
  * Also add #[AsMapper(from: Foo:class, to: Bar:class)] to each mapper class.
  *
+ * @template TFrom of object
+ * @template TTo of object
+ *
  * @author Ryan Weaver <ryan@symfonycasts.com>
  */
 interface MapperInterface
 {
+    /**
+     * @param TFrom $from
+     * @param class-string<TTo> $toClass
+     * @param array $context
+     * @return TTo
+     */
     public function load(object $from, string $toClass, array $context): object;
 
+    /**
+     * @param TFrom $from
+     * @param TTo $to
+     * @param array $context
+     * @return TTo
+     */
     public function populate(object $from, object $to, array $context): object;
 }
--- src/MicroMapperInterface.php
+++ src/MicroMapperInterface.php
@@ -11,10 +11,19 @@ namespace Symfonycasts\MicroMapper;
 
 /**
  * Maps one object to another using the configured mappers.
+ *
+ * @template TFrom of object
+ * @template TTo of object
  */
 interface MicroMapperInterface
 {
     public const MAX_DEPTH = 'max_depth';
 
+    /**
+     * @param TFrom $from
+     * @param class-string<TTo> $toClass
+     * @param array $context
+     * @return TTo
+     */
     public function map(object $from, string $toClass, array $context = []): object;
 }
